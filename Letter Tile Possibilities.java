dcf7e622fb2318c95803a3de96ce08fad24aed37
Abhishek Gupta <148879663+wiz-abhi@users.noreply.github.com>
2025-11-07 00:03:16 +0530

    Refactor numTilePossibilities to use backtracking

--- Letter Tile Possibilities.java
+++ Letter Tile Possibilities.java
@@ -1,19 +1,21 @@
-class Solution {
+public class Solution {
     public int numTilePossibilities(String tiles) {
-        int[] cnt = new int[26];
-        for (char c : tiles.toCharArray()) cnt[c - 'A']++;
-        return dfs(cnt);
+        int[] count = new int[26];
+        for (char c : tiles.toCharArray()) {
+            count[c - 'A']++;
+        }
+        return backtrack(count);
     }
 
-    private int dfs(int[] cnt) {
-        int total = 0;
+    private int backtrack(int[] count) {
+        int sum = 0;
         for (int i = 0; i < 26; i++) {
-            if (cnt[i] == 0) continue;
-            total++;          // using this letter creates a new non-empty sequence
-            cnt[i]--;         // choose
-            total += dfs(cnt);// extend
-            cnt[i]++;         // backtrack
+            if (count[i] == 0) continue;
+            sum++;
+            count[i]--;
+            sum += backtrack(count);
+            count[i]++;
         }
-        return total;
+        return sum;
     }
 }
