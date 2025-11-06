4184475416266a1ae251361ca364feef0c6555f7
Abhishek Gupta <148879663+wiz-abhi@users.noreply.github.com>
2025-11-06 22:59:35 +0530

    Update Delete Mid of a Stack.java

--- Delete Mid of a Stack.java
+++ Delete Mid of a Stack.java
@@ -1,1 +1,22 @@
+//Delete Mid of a Stack
 
+import java.util.Stack;
+
+class Solution {
+    public void deleteMid(Stack<Integer> s) {
+        int len = s.size();
+        deleteMid(s, len, 0);
+    }
+
+    void deleteMid(Stack<Integer> st, int size, int currSize) {
+        // base case: reached middle from top
+        if (currSize == size / 2) {
+            st.pop();
+            return;
+        }
+
+        int top = st.pop();
+        deleteMid(st, size, currSize + 1);
+        st.push(top); // backtrack
+    }
+}
